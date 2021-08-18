a3df3a5e67967f5bde62118f32fa6949b8bc32a7
WilsonHan <Win05118@gmail.com>
2021-08-19 04:30:27 +0800

    20210819-v1

---
+++ app/assets/js/all.js
@@ -0,0 +1,18 @@
+// $(document).ready(function() {
+//   $( "#buy" ).click(function() {
+//     $(".bbb").toggleClass("d-none");
+//   });
+// });
+
+// $(function(){
+//   $( "#buy" ).click(function() {
+//     $("#bbb").toggleClass("d-none");
+//   });
+// });
+
+// $(document).ready(function() {
+//   $(".menu a").click(function() {
+//     preventdefault();
+//     $(".menu a").toggleClass("active");
+//   });
+// });
